267646c0bb60344c804f1dd5ec1aee3d5059a8a9
Ember Doyle <doyle.ember@gmail.com>
2024-10-03 11:18:26 -0600

    add append method for Linked List

--- linkedlist.js
+++ linkedlist.js
@@ -1,11 +1,34 @@
-const LinkedList = (function(){
-    //represents full list
-    
-
-})();
-
 //node constructor
-function Node (value,next){
-    this.value = value;
-    this.next = next;
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    //list constructor
+    constructor() {
+        this.head = null;
+    }
+    append(data) {
+        //create node
+        const newNode = new Node(data);
+        //if list is empty, assign this node to head
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            //traveses the list until it reaches last node and appends new node
+            //start at head
+            let current = this.head;
+            //search until there is a node with a null next
+            while (current.next !== null) {
+                //move to next node
+                current = current.next;
+            }
+            //whn the while is finished ie when there is a next = null, add this new node as the next
+            current.next = newNode;
+        }
+    }
 };
+
